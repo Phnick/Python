7ec185d2a762cbd4007f83c358dfd0133d1c52d1
Phnick <115507431+Phnick@users.noreply.github.com>
2024-05-01 11:08:00 -0300

    Add files via upload
    Aprendendo Python

---
+++ python/coletar.py
@@ -0,0 +1,27 @@
+
+##Desafio 1
+nome =(input('Qual e seu nome: '))
+idade =(input('Qual a sua idade: '))
+altura =(input('Qual é sua altura: '))
+print(f'Olá {nome}, você tem {idade} anos e mede {altura} metros')
+
+##Calculadora com operadores
+
+valor1 =int(input('Olá pessoa 1, insira um valor numérico: '))
+valor2 =int(input('Olá pessoa 2, insira um valor numérico'))
+soma_total = valor1 + valor2
+
+print(f'A soma do valor  {valor1} + o valor  {valor2} é igual a {soma_total}')
+
+
+##Editando textos
+frase = ' Eu amo meu cachorro'
+print(frase)
+
+frase2 = 'Eu amo meu cachorro'
+print(frase.upper())
+
+nome = str(input( 'Qual o nome do seu cachorro?: '))
+print(nome.upper())
+
+
